af2327af56682f796ab20c0bd51948b024da2824
Mia! <165869037+mia-odea@users.noreply.github.com>
2024-04-18 10:48:08 +0800

    Updated signup.js to redirect and include log in
    Added log in functionality to signup.js and included `window.location.href = '/forum';` which redirects to a website called forum (will change to home webpage when we make it). This occurs after a successful log in or sign up

--- static/signup.js
+++ static/signup.js
@@ -7,11 +7,33 @@ $(document).ready(function() {
             data: $('#signup-form').serialize(),
             success: function(response) {
                 if (response.error) {
-                    $('#message').html(response.error);
+                    $('#Smessage').html(response.error);
                     console.log(response);
                 } else {
-                    $('#message').html(response.success);
+                    $('#Smessage').html(response.success);
+                    window.location.href = '/forum';
                     $('#signup-form')[0].reset();
+                    console.log(response);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        });
+    });
+    $('#login-form').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/login',
+            data: $('#login-form').serialize(),
+            success: function(response) {
+                if (response.error) {
+                    $('#Lmessage').html(response.error);
+                    console.log(response);
+                } else if (response.success) {  
+                    $('#Lmessage').html(response.success);
+                    window.location.href = '/forum';
                     console.log(response);
                 }
             },
