ea8a03b08550fe6ac12926e0306bdc4887fa8612
Mia! <165869037+mia-odea@users.noreply.github.com>
2024-04-27 13:15:50 +0800

    Create signup.js
    signup.js validates input while user is creating new account

---
+++ app/static/scripts/signup.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // validate username using validation.js
+    $("#username").blur(function() {
+        validateNewUsername(this.value);
+    });
+    // validate email using validation.js
+    $("#email").blur(function() {
+        validateEmail(this.value);
+    });
+    // validates that password isn't blank
+    $("#password").blur(function() {
+        let $password2 = $("#password2").val();
+
+        if(this.value.length === 0) {
+            displayText("Enter password", "error");
+        }
+    
+    // validates that confirm password is the same and isn't blank
+    $("#password2").blur(function() {
+        let $password = $("#password").val();
+        if(this.value.length === 0) {
+            displayText("Confirm password", "error");
+        }
+        else if(this.value != $password) {
+            displayText("Passwords don't match", "error");
+        } else {
+            clearText();
+        }
+    });
+});
+  
