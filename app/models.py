da01555df056b7c6e15c57c598ba9770ab80af88
Eunice Sha <eunice.sha@hotmail.com>
2024-05-13 16:53:56 +0800

    - Created a very basic leaderboard html - updated users table in models.py to contain points - updated the game logic to update point system on leader board - updated bse.html so there is a leaderboard option on the header nav bar
    what needs work:
    css for leaderboard.html
    adding the points on to the profile page as well

--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+    
+    points: so.Mapped[int] = so.mapped_column(sa.Integer, default=0, nullable=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
